56fb0cb246ba772d58ebd4182fe66b56884dc4aa
Anders Eriksson <digitaldoktorn@gmail.com>
2019-04-22 13:23:52 +0200

    Laravel basics del 14

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 @section('content')
 
-    <form action="">
+    <form action="{{ url('posts') }}" method="POST">
+        {{ csrf_field() }}
         <label for="title">Title</label>
         <input id="title" type="text" name="title">
 
         <label for="content">Content</label>
         <input id="content" type="text" name="content">
+
+        <button type="submit">Create</button>
     </form>
 
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 @section('content')
 
+    <a href="{{ url('posts/create') }}">Create new post</a><br>
+
     @foreach ($posts as $post)
         <a href="{{ url('posts/'.$post->id) }}">
         {{ $post->title }}</a><br>
+        <a href="">Edit</a>
+        <hr>
     @endforeach
 
 @endsection
