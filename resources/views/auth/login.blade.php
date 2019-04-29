dfbbe41cc2c96e743c9f541db27a76bae53807ba
Anders Eriksson <digitaldoktorn@gmail.com>
2019-04-29 21:31:42 +0200

    made login page the start page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Human Rights Focus | Registered members access only') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -38,18 +38,6 @@
                                 @endif
                             </div>
                         </div>
-
-                        {{--<div class="form-group row">--}}
-                            {{--<div class="col-md-6 offset-md-4">--}}
-                                {{--<div class="form-check">--}}
-                                    {{--<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>--}}
-
-                                    {{--<label class="form-check-label" for="remember">--}}
-                                        {{--{{ __('Remember Me') }}--}}
-                                    {{--</label>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,21 @@
 Route::get('/', 'PostController@index');
 Route::get('/board', 'BoardmemberController@index');
 
-//Route::get('/board', 'PageController@board');
-
 Route::get('/localcontacts', 'PageController@localcontacts');
 
 Route::resource('posts', 'PostController');
 Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Används denna?
+//Route::get('/home', 'HomeController@index')->name('home');
+
 
 Route::resource('boardmembers', 'BoardmemberController');
+
+// makes the login page the start page
+Route::group(['middleware' => ['web', 'auth']], function() {
+
+    Route::get('/', function () {
+        return view('/auth/login');
+    });
+});
