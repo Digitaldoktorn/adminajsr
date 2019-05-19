df9a9136a3e0a6d9f62d620291da13e73e4df586
Anders Eriksson <digitaldoktorn@gmail.com>
2019-05-19 17:23:19 +0200

    removed unused tables, created Roles model and migration, migrate fresh

--- database/migrations/2019_05_19_144358_create_roles_table.php
+++ database/migrations/2019_05_19_144358_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserRolesTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateUserRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_roles', function (Blueprint $table) {
-            $table->bigIncrements('role_id');
-            $table->string('name');
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateUserRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_roles');
+        Schema::dropIfExists('roles');
     }
 }
